94cee5a9802b378579d0c9dee556bc0db25ff50f
Jacque <jacquelinegez@icloud.com>
2019-05-11 17:31:54 -0400

    fixed files

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name VARCHAR (50) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (150),
+    devoured BOOLEAN DEFAULT,
+    PRIMARY KEY 
 );
