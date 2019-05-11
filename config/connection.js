7ce2c2cda198760f9332275a1af5bedeb31778de
Jacque <jacquelinegez@icloud.com>
2019-05-11 13:29:01 -0400

    Added Files

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password ",
+    database: "burgers_db"
+});
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected ad id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burger_db;
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name) VALUES ('Blackbean Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Hamburger', false);
