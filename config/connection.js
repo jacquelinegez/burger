63a7ed505dc8f5df590367b4baa57498e6ea22f3
Jacque <jacquelinegez@icloud.com>
2019-05-12 20:41:42 -0400

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,24 @@
 var mysql = require("mysql");
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSONDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port:3000,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+     });
+}
+connection.connect(); 
+/*
 connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "password ",
+    password: "password",
     database: "burgers_db"
-});
+});  
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
@@ -12,5 +26,5 @@ connection.connect(function(err) {
     }
     console.log("connected ad id " + connection.threadId);
 });
-
+*/
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR (150),
-    devoured BOOLEAN DEFAULT,
-    PRIMARY KEY 
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY  (id)
 );
